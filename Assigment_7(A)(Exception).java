44845c0519719a15de2c5e6b148142d4f9ea9d1a
Prasanna Pandhare <114733798+Prasannapandhare@users.noreply.github.com>
2023-05-17 16:42:30 +0530

    Update Assigment_7(A)(Exception).java

--- Assigment_7(A)(Exception).java
+++ Assigment_7(A)(Exception).java
@@ -19,7 +19,7 @@ public class division {
 			System.out.println("Remainder : "+r);
 			
 			int arr[] = {1,2,3,4,5,6};
-			for(int i=0;i<arr.length;i++)
+			for(int i=0;i<=arr.length;i++)
 			{
 				System.out.println(arr[i]+"");
 			}
